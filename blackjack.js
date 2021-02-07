92dca3e60e79f02b89d5430cd9e62d432d34855e
Sara Bertuldo <sarabertuldo@gmail.com>
2021-02-06 20:22:22 -0600

    better layout

--- blackjack.js
+++ blackjack.js
@@ -1,50 +1,21 @@
-let dealerChoice;
-let playerChoice;
-let stats = {
-    win: 0,
-    loss: 0,
-    tie: 0,
-};
+let gameStarted = false;
 
-// check to see if there are missing inputs from user, refer to formValidation
-// let errors = {
-//     name: true,
-//     birthday: true,
-// }
+let suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let deck = new Array();
 
-// let nameInput = document.getElementById("nameInput").value;
-// let birthYear = document.getElementById("birthYear").value;
-// 
-// document.getElementById("username")
-// .addEventListener("blur", (e) => { 
-//     if (document.getElementById("nameInput").value.length < 0) {
-//         document.getElementById("errorMsg1").innerText =
-//         "Please enter name.";
-//         errors = true;
-//     } if (document.getElementById("birthYear").value.length >= 0) {
-//         document.getElementById("errorMsg2").innerText = 
-//         "Please enter birth year.";
-//         errors = true;
-//     }
-//     }
-// );
-// 
-// let suits = ["clubs", "diamonds", "hearts", "spades"];
-// let cardValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+let playGame = document.getElementById("playGame");
+let hit = document.getElementById("hit");
+let stay = document.getElementById("stay");
 
+playGame.addEventListener("click", function () {
+    console.log("Play Game");
+})
 
-// submit information, but check to see if user is appropriate age
-document.getElementById("submit")
-.addEventListener("click", function () {
-let nameInput = document.getElementById("nameInput").value;
-    if (birthYear.value < 2000) {
-    window.location.href = "blackjackgame.html"
-    }
-    else {
-    window.location.href = "http://www.google.com"
-    }});
+hit.addEventListener("click", function () {
+    console.log("Hit");
+})
 
-
-// winner outcome
-// function setOutcome () {}
-// ${nameInput} wins!
+stay.addEventListener("click", function () {
+    console.log("Stay");
+})
