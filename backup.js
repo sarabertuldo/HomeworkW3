d42542197fcda77698ed6e4962b297abd0c7b1fb
Sara Bertuldo <sarabertuldo@gmail.com>
2021-02-07 18:33:01 -0600

    js

--- backup.js
+++ backup.js
@@ -3,15 +3,16 @@ let faces = [{value: 11, face: "Ace",}, {value: 2, face: "Two"}, {value: 3, face
 {value: 5, face: "Five"}, {value: 6, face: "Six"}, {value: 7, face: "Seven"}, {value: 8, face: "Eight"},
 {value: 9, face: "Nine"}, {value: 10, face: "Ten"}, {value: 10, face: "Jack"}, {value: 10, face: "Queen"},
 {value: 10, face: "King"}];
+let deck = [];
 
 // start game by building the deck
 function buildDeck() {
-    let deck = [];
+    let deck = new Array();
     for (let i = 0; i < suits.length; i++)
     {
-        for (let j = 0; j < faces.length; j++)
+        for (let x = 0; x < faces.length; x++)
         {
-            let card = {Value: values[j], Suit: suits[i]};
+            let card = {Value: values[x], Suit: suits[i]};
             deck.push(card);
         }
         }
@@ -20,17 +21,11 @@ function buildDeck() {
 
 // shuffle deck
 function shuffleDeck() {
-        for (let i = 0; i < deck.length; i++)
+        for (let i = deck.length - 1; i > 0; i--)
         {
             let j = Math.floor(Math.random() * i);
             let temp = deck[i]
             deck[i] = deck[j];
             deck[j] = temp;
     }
-}
-
-// play game
-function playGame () 
-{
-    document.getElementById('playGame')
 }
